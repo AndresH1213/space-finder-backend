3ed0371b5bd5826b3d9bec93b4248c6ba5e4f781
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-02 17:50:42 -0500

    creation of apigateway and generic table stack

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -5,16 +5,26 @@ import {
   Function as LambdaFunction,
   Runtime,
 } from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { join } from 'path';
+import { GenericTable } from './GenericTable';
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, 'SpaceApi');
+  private spacesTable = new GenericTable('SpacesTable', 'spaceId', this);
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    new LambdaFunction(this, 'helloLambda', {
+    const helloLambda = new LambdaFunction(this, 'helloLambda', {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main',
     });
+
+    // Helllo Api lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
