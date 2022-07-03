ae0d13d5a2bce207053dca52fe0d64cbcf14778b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-03 13:11:00 -0500

    Lambdas with typescript, generic table first approach

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -8,6 +8,8 @@ import {
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { join } from 'path';
 import { GenericTable } from './GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
@@ -16,14 +18,18 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_16_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main',
+    const helloLambdaNodejs = new NodejsFunction(this, 'helloLambdaNodejs', {
+      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+      handler: 'handler',
     });
 
-    // Helllo Api lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions('s3:ListAllMyBuckets');
+    s3ListPolicy.addResources('*');
+    helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
+
+    // Hello Api lambda integration
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
