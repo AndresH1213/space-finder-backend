e55c2911aff6add680e12d95da93e24462e384ed
Andres Hernandez <edanhebla1213@hotmail.com>
2022-07-21 21:28:37 -0500

    authorizer lambda to access the apigateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,20 +1,24 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Fn, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {
   AuthorizationType,
-  LambdaIntegration,
   MethodOptions,
   RestApi,
 } from 'aws-cdk-lib/aws-apigateway';
 import { join } from 'path';
-import { GenericTable } from './GenericTable';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
+
 import { AuthorizerWrapper } from './auth/AuthorizerWrapper';
+import { GenericTable } from './GenericTable';
+import { WebAppDeployment } from './WebAppDeployment';
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, 'SpaceApi');
   private authorizer: AuthorizerWrapper;
+  private suffix?: string;
+  private spacesPhotosBucket?: Bucket;
+
   private spacesTable = new GenericTable(this, {
     tableName: 'SpacesTable',
     primaryKey: 'spaceId',
@@ -28,17 +32,18 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    this.authorizer = new AuthorizerWrapper(this, this.api);
-
-    const helloLambdaNodejs = new NodejsFunction(this, 'helloLambdaNodejs', {
-      entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
-      handler: 'handler',
-    });
+    this.initializeSuffix();
+    this.initilizeSpacesPhotosBucket();
+    this.authorizer = new AuthorizerWrapper(
+      this,
+      this.api,
+      this.spacesPhotosBucket?.bucketArn + '/*'
+    );
+    new WebAppDeployment(this, this.suffix!);
 
     const s3ListPolicy = new PolicyStatement();
     s3ListPolicy.addActions('s3:ListAllMyBuckets');
     s3ListPolicy.addResources('*');
-    helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
 
     const optionsWithAuthorizer: MethodOptions = {
       authorizationType: AuthorizationType.COGNITO,
@@ -47,20 +52,49 @@ export class SpaceStack extends Stack {
       },
     };
 
-    // Hello Api lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
-    const helloLambdaResource = this.api.root.addResource('hello');
-    helloLambdaResource.addMethod(
-      'GET',
-      helloLambdaIntegration,
-      optionsWithAuthorizer
-    );
-
     // Spaces API integrations:
     const spaceResource = this.api.root.addResource('spaces');
-    spaceResource.addMethod('POST', this.spacesTable.createLambdaIntegration);
-    spaceResource.addMethod('GET', this.spacesTable.readLambdaIntegration);
-    spaceResource.addMethod('PUT', this.spacesTable.updateLambdaIntegration);
-    spaceResource.addMethod('DELETE', this.spacesTable.deleteLambdaIntegration);
+    spaceResource.addMethod(
+      'POST',
+      this.spacesTable.createLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    spaceResource.addMethod(
+      'GET',
+      this.spacesTable.readLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    spaceResource.addMethod(
+      'PUT',
+      this.spacesTable.updateLambdaIntegration,
+      optionsWithAuthorizer
+    );
+    spaceResource.addMethod(
+      'DELETE',
+      this.spacesTable.deleteLambdaIntegration,
+      optionsWithAuthorizer
+    );
+  }
+
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+    const Suffix = Fn.select(4, Fn.split('-', shortStackId));
+    this.suffix = Suffix;
+  }
+
+  private initilizeSpacesPhotosBucket() {
+    this.spacesPhotosBucket = new Bucket(this, 'spaces-photos', {
+      bucketName: 'spaces-photos-' + this.suffix,
+      cors: [
+        {
+          allowedMethods: [HttpMethods.HEAD, HttpMethods.GET, HttpMethods.PUT],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+        },
+      ],
+    });
+    new CfnOutput(this, 'spaces-photos-bucket-name', {
+      value: this.spacesPhotosBucket.bucketName,
+    });
   }
 }
